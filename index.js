c8f6fe114fa1b18ccc896b00995e3ec54f6678c5
Jon Dale <jonners@gmail.com>
2015-12-14 22:38:19 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    request = require('request'),
+    parser = require('xml2json'),
+    app = express(),
+    server;
+
+//Configure middleware
+app.use(function (req, res, next) {
+  if(app.get('env') === 'development') {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  }
+  next();
+})
+
+//Search routes
+app.get('/jobsearch/github', function(req, res) {
+  request({url: 'https://jobs.github.com/positions.json',
+           json: true,
+           qs: req.query}, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  })
+});
+
+//Start server
+server = app.listen(3000, function() {
+  var host = server.address().address,
+      port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
